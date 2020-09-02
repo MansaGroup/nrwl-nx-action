85a35c0c9c9f6c676fb012a507613f6c8ec2cad6
Jérémy Levilain <jeremy.levilain@epitech.eu>
2020-09-02 18:42:29 +0200

    docs: add README.md and LICENSE.txt

--- script.sh
+++ script.sh
@@ -5,17 +5,31 @@ then
     yarn global add @nrwl/cli
 fi
 
-if [[ "$INPUT_ALL" == "true" ]] || [[ "$INPUT_AFFECTED" == "false" ]]
+if [[ "$INPUT_PARALLEL" == "true" ]]
+then
+    INPUT_AFFECTED="--parallel $INPUT_AFFECTED"
+fi
+
+if [[ "$INPUT_PROJECTS" != "" ]]
+then
+    for project in $(echo $INPUT_PROJECTS | sed "s/,/ /g")
+    do
+        for target in $(echo $INPUT_TARGETS | sed "s/,/ /g")
+        do
+            nx $target $project $INPUT_ARGS
+        done
+    done
+elif [[ "$INPUT_ALL" == "true" ]] || [[ "$INPUT_AFFECTED" == "false" ]]
 then
     for target in $(echo $INPUT_TARGETS | sed "s/,/ /g")
     do
-        nx run-many --target=$target --all $
+        nx run-many --target=$target --all $INPUT_ARGS
     done
 else
     if [[ $GITHUB_BASE_REF ]]
     then
-        export NX_BASE=${{ github.event.pull_request.base.sha }}
-        export NX_HEAD=${{ github.event.pull_request.head.sha }}
+        export NX_BASE=$GITHUB_BASE_REF
+        export NX_HEAD=$GITHUB_HEAD_REF
     else
         export NX_BASE=$(git rev-parse HEAD~1)
         export NX_HEAD=$(git rev-parse HEAD)
